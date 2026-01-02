817754f6c8055f4711eee87115563c81f03f96dd
EdHungria <edimarhungria09@gmail.com>
2026-01-02 12:24:36 -0300

    Create Index.xhtml.java
    Código do Index.xhtml

---
+++ Index.xhtml.java
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:h="http://xmlns.jcp.org/jsf/html">
+<h:head>
+    <title>Vacinas</title>
+    <meta http-equiv="refresh" content="0;url=animal.xhtml" />
+</h:head>
+<h:body>
+    <p>Redirecionando para gerenciamento de animais...</p>
+</h:body>
+</html>
