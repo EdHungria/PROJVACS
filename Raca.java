9d7e5fc52783a27f89eabb972928f244b7457459
EdHungria <edimarhungria09@gmail.com>
2026-01-02 12:08:53 -0300

    Create Raca.java
    Código de Entidade Raca

---
+++ Raca.java
@@ -0,0 +1,68 @@
+package br.edu.ufra.entidade;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Raca.findAll", query = "SELECT r FROM Raca r"),
+        @NamedQuery(name = "Raca.findById", query = "SELECT r FROM Raca r WHERE r.id = :id")
+})
+public class Raca implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    public Raca() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Raca)) {
+            return false;
+        }
+        Raca other = (Raca) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
