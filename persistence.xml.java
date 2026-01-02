8c89cd4f39352721ceacdfca45b8f913fa58a7b2
EdHungria <edimarhungria09@gmail.com>
2026-01-02 12:18:00 -0300

    Create persistence.xml.java
    código do resource persistente.xml

---
+++ persistence.xml.java
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<persistence version="2.1"
+             xmlns="http://xmlns.jcp.org/xml/ns/persistence"
+             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+             xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/persistence
+                                 http://xmlns.jcp.org/xml/ns/persistence/persistence_2_1.xsd">
+
+    <persistence-unit name="VacinasPU" transaction-type="RESOURCE_LOCAL">
+        <provider>org.eclipse.persistence.jpa.PersistenceProvider</provider>
+
+        <class>br.edu.ufra.entidade.Raca</class>
+        <class>br.edu.ufra.entidade.Animal</class>
+        <class>br.edu.ufra.entidade.Vacina</class>
+        <class>br.edu.ufra.entidade.Usuario</class>
+
+        <properties>
+            <property name="javax.persistence.jdbc.url" value="jdbc:mysql://localhost:3306/vacinas"/>
+            <property name="javax.persistence.jdbc.user" value="root"/>
+            <property name="javax.persistence.jdbc.password" value="root"/>
+            <property name="javax.persistence.jdbc.driver" value="com.mysql.cj.jdbc.Driver"/>
+
+            <!-- Log mais detalhado para ajudar na depuração -->
+            <property name="eclipselink.logging.level" value="FINE"/>
+            <property name="eclipselink.logging.parameters" value="true"/>
+
+            <!-- Não criar tabelas automaticamente (já temos os scripts SQL executados) -->
+            <property name="eclipselink.ddl-generation" value="none"/>
+        </properties>
+    </persistence-unit>
+</persistence>
